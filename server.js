653ac74b38be35954363e2960300bf3eb15df518
FaizanHaiider <faizanhaider12@gmail.com>
2018-08-15 13:19:01 -0400

    updated server.js to connect to db using mongoURI and return a success/fail msg

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 // requirements
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+const db = require("./config/keys").mongoURI;
 const port = process.env.PORT || 5000;
+
+// connect to mongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("db connected..."))
+  .catch(err => console.log(err));
 
 // initial root route
 app.get("/", (req, res) => res.send("HelloWorld"));
